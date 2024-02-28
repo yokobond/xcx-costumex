7c00136f4a4ad0f448960cd06ae628c5b43f3f0c
yokobond <koji.yokokawa@gmail.com>
2024-02-28 15:58:54 +0900

    Implement all blocks

--- src/vm/extensions/block/index.js
+++ src/vm/extensions/block/index.js
@@ -5,6 +5,9 @@ import log from '../../util/log';
 import translations from './translations.json';
 import blockIcon from './block-icon.png';
 
+import {checkDebugMode} from './dev-util.js';
+import {addImageAsCostume, getCostumeIndexByNameOrNumber} from './costume-util';
+
 /**
  * Formatter which is used for translation.
  * This will be replaced which is used in the runtime.
@@ -98,6 +101,13 @@ class ExtensionBlocks {
             // Replace 'formatMessage' to a formatter which is used in the runtime.
             formatMessage = runtime.formatMessage;
         }
+        runtime.on('EXTENSION_ADDED', this.onExtensionAdded.bind(this));
+    }
+
+    onExtensionAdded (extensionInfo) {
+        if (extensionInfo.id === EXTENSION_ID) {
+            checkDebugMode();
+        }
     }
 
     /**
@@ -113,33 +123,276 @@ class ExtensionBlocks {
             showStatusButton: false,
             blocks: [
                 {
-                    opcode: 'do-it',
+                    opcode: 'takeSnapshot',
                     blockType: BlockType.REPORTER,
                     blockAllThreads: false,
                     text: formatMessage({
-                        id: 'costumex.doIt',
-                        default: 'do it [SCRIPT]',
-                        description: 'execute javascript for example'
+                        id: 'costumex.takeSnapshot',
+                        default: 'snapshot x:[X] y:[Y] w:[WIDTH] h:[HEIGHT]',
+                        description: 'costumex takeSnapshot text'
                     }),
-                    func: 'doIt',
+                    func: 'takeSnapshot',
                     arguments: {
-                        SCRIPT: {
+                        X: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        Y: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        WIDTH: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 480
+                        },
+                        HEIGHT: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 360
+                        }
+                    }
+                },
+                {
+                    opcode: 'addImageAsCostume',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'costumex.addImageAsCostume',
+                        default: 'add image [DATA] as costume [NAME]',
+                        description: 'CostumeX addImageAsCostume text'
+                    }),
+                    func: 'addImageAsCostume',
+                    arguments: {
+                        DATA: {
                             type: ArgumentType.STRING,
-                            defaultValue: '3 + 4'
+                            defaultValue: 'data:image/png;base64,'
+                        },
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: formatMessage({
+                                id: 'costumex.addImageAsCostume.defaultCostumeName',
+                                default: 'costume',
+                                description: 'CostumeX addImageAsCostume defaultCostumeName text'
+                            })
+                        }
+                    }
+                },
+                {
+                    opcode: 'deleteCostume',
+                    blockType: BlockType.COMMAND,
+                    text: formatMessage({
+                        id: 'costumex.deleteCostume',
+                        default: 'delete costume [COSTUME]',
+                        description: 'CostumeX deleteCostume text'
+                    }),
+                    func: 'deleteCostume',
+                    arguments: {
+                        COSTUME: {
+                            type: ArgumentType.COSTUME,
+                            menu: 'costumeNamesMenu'
+                        }
+                    }
+                },
+                {
+                    opcode: 'costumesLength',
+                    blockType: BlockType.REPORTER,
+                    text: formatMessage({
+                        id: 'costumex.costumesLength',
+                        default: 'costumes length',
+                        description: 'CostumeX costumesLength text'
+                    }),
+                    func: 'costumesLength'
+                },
+                {
+                    opcode: 'costumeNameAt',
+                    blockType: BlockType.REPORTER,
+                    text: formatMessage({
+                        id: 'costumex.costumeNameAt',
+                        default: 'costume name at [INDEX]',
+                        description: 'CostumeX costumeNameAt text'
+                    }),
+                    func: 'costumeNameAt',
+                    arguments: {
+                        INDEX: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'costumeData',
+                    blockType: BlockType.REPORTER,
+                    text: formatMessage({
+                        id: 'costumex.costumeData',
+                        default: 'image data of costume [COSTUME]',
+                        description: 'CostumeX costumeData text'
+                    }),
+                    func: 'costumeData',
+                    arguments: {
+                        COSTUME: {
+                            type: ArgumentType.COSTUME,
+                            menu: 'costumeNamesMenu'
                         }
                     }
                 }
             ],
             menus: {
+                costumeNamesMenu: {
+                    acceptReporters: true,
+                    items: 'getCostumeNamesMenu'
+                }
             }
         };
     }
 
-    doIt (args) {
-        const statement = Cast.toString(args.SCRIPT);
-        const func = new Function(`return (${statement})`);
-        log.log(`doIt: ${statement}`);
-        return func.call(this);
+    getCostumeNamesMenu () {
+        const menu = [];
+        const target = this.runtime.getEditingTarget();
+        if (!target) {
+            return menu;
+        }
+        const costumes = target.sprite.costumes;
+        for (let i = 0; i < costumes.length; i++) {
+            menu.push({
+                text: costumes[i].name,
+                value: costumes[i].name
+            });
+        }
+        return menu;
+    }
+
+    /**
+     * Take a snapshot.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     * @returns {Promise<string>} - a Promise that resolves snapshot data URL
+     */
+    takeSnapshot (args, util) {
+        const x = Cast.toNumber(args.X);
+        const y = Cast.toNumber(args.Y);
+        const width = Cast.toNumber(args.WIDTH);
+        const height = Cast.toNumber(args.HEIGHT);
+        const runtime = util.runtime;
+        return new Promise(resolve => {
+            this.runtime.renderer.requestSnapshot(imageDataURL => {
+                resolve(` ${imageDataURL} `);
+            });
+        })
+            .then(imageDataUrl => new Promise((resolve, reject) => {
+                const image = new Image();
+                image.onload = function () {
+                    resolve(image);
+                    image.onload = null;
+                    image.onerror = null;
+                };
+                image.onerror = function () {
+                    reject(new Error('Costume load failed. Asset could not be read.'));
+                    image.onload = null;
+                    image.onerror = null;
+                };
+                image.src = imageDataUrl;
+            }))
+            .then(imageElem => {
+                const bitmapResolution = 2;
+                const outputWidth = 480 * bitmapResolution;
+                const outputHeight = 360 * bitmapResolution;
+                const [stageWidth, stageHeight] = runtime.renderer.getNativeSize();
+                const stepPixelRatioWidth = imageElem.width / stageWidth;
+                const stepPixelRatioHeight = imageElem.height / stageHeight;
+                const canvas = document.createElement('canvas');
+                canvas.width = (width / stageWidth) * outputWidth;
+                canvas.height = (height / stageHeight) * outputHeight;
+                const context = canvas.getContext('2d');
+                context.drawImage(
+                    imageElem,
+                    ((stageWidth / 2) + x - (width / 2)) * stepPixelRatioWidth,
+                    ((stageHeight / 2) - (y + (height / 2))) * stepPixelRatioHeight,
+                    width * stepPixelRatioWidth,
+                    height * stepPixelRatioHeight,
+                    0,
+                    0,
+                    canvas.width,
+                    canvas.height);
+                return ` ${canvas.toDataURL()} `;
+            })
+            .catch(error => {
+                log.error(error);
+                return error.message;
+            });
+    }
+
+    /**
+     * Add an image as a costume.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     * @returns {Promise<string>} - a Promise that resolves when the image is added then returns the data URL
+     */
+    addImageAsCostume (args, util) {
+        const target = util.target;
+        const dataURL = Cast.toString(args.DATA).trim();
+        const imageName = Cast.toString(args.NAME);
+        const runtime = this.runtime;
+        return addImageAsCostume(target, dataURL, runtime, imageName, runtime.vm)
+            .then(costume => ` ${costume.asset.encodeDataURI()} `)
+            .catch(error => {
+                log.error(error);
+                return error.message;
+            });
+    }
+
+    /**
+     * Delete a costume.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     */
+    deleteCostume (args, util) {
+        const target = util.target;
+        if (target.sprite.costumes.length <= 1) {
+            // Must have at least one costume
+            return;
+        }
+        const costumeName = Cast.toString(args.COSTUME);
+        const costumeIndex = getCostumeIndexByNameOrNumber(target, costumeName);
+        if (costumeIndex === null) return;
+        target.deleteCostume(costumeIndex);
+    }
+
+    /**
+     * Get the number of costumes.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     * @returns {number} - the number of costumes.
+     */
+    costumesLength (args, util) {
+        return util.target.sprite.costumes.length;
+    }
+
+    /**
+     * Get the name of a costume at a given index.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     * @returns {string} - the name of the costume.
+     */
+    costumeNameAt (args, util) {
+        const index = Cast.toNumber(args.INDEX);
+        const target = util.target;
+        const costumes = target.sprite.costumes;
+        if (index < 1 || index > costumes.length) {
+            return '';
+        }
+        return costumes[index - 1].name;
+    }
+
+    /**
+     * Get the image data of a costume.
+     * @param {object} args - the block's arguments.
+     * @param {object} util - utility object provided by the runtime.
+     * @returns {string} - the image data of the costume.
+     */
+    costumeData (args, util) {
+        const target = util.target;
+        const costumeName = Cast.toString(args.COSTUME);
+        const costumeIndex = getCostumeIndexByNameOrNumber(target, costumeName);
+        if (costumeIndex === null) return '';
+        return ` ${target.getCostumes()[costumeIndex].asset.encodeDataURI()} `;
     }
 }
 
